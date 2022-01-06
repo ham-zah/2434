1867a9be540fca7c8ead2928e54bf5012d79491f
ham-zah <90850092+ham-zah@users.noreply.github.com>
2022-01-06 20:28:04 +0700

    Delete load0.js

--- js/load0.js
+++
@@ -1,5 +0,0 @@
-/* Loader */
-var loadID = doc.getElementById("load"),
-    loadRW = doc.createElement("div");
-loadRW.innerHTML='<div class="nload text-center"><div class="dots_0"><div class="gobblue"></div><div class="gobred"></div><div class="gobyellow"></div><div class="gobgreen"></div></div></div>';
-loadID.parentNode.replaceChild(loadRW,loadID);
