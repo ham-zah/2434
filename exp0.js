996dfd11ebfc5a8de34cfc321ef7f5233d0c697a
ham-zah <90850092+ham-zah@users.noreply.github.com>
2022-01-06 20:48:32 +0700

    Update and rename exp0.js to exp1.js

--- exp0.js
+++
@@ -1,2 +0,0 @@
-loadyn(cdns+"gh/ham-zah/2434/main/css/exp0.min.css","css");
-loadyn(cdns+"gh/ham-zah/2434/main/js/exp0.min.js","js");
---
+++ exp1.js
@@ -0,0 +1,2 @@
+loadyn(cdns+gh0+"css/exp1.min.css","css");
+loadyn(cdns+gh0+"js/exp0.min.js","js");
