10373cf0a87da23c57568f201094e9a4d9f658a9
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-01-13 19:54:49 +0700

    Update and rename exp0.js to exp1.js

--- js/exp1.js
+++ js/exp1.js
@@ -1,7 +1,8 @@
 var nd = [
   "example.com",
   "teratasi.com",
-  "foxpop.co.uk"
+  "foxpop.co.uk",
+  "phosfor.co"
 ];
 
 var exp= [
@@ -22,7 +23,7 @@ var exp= [
   "#EDU",
   "#GOV"
 ];
-  
+/* EXAMPLE */
 if(ls.indexOf(nd[0]+"&d=0")>-1)
 exp=[nd[0],
   "65.000",
@@ -41,7 +42,7 @@ exp=[nd[0],
   "10",
   "10"
 ];
-
+/* TERATASI.COM */
 if(ls.indexOf(nd[1]+"&d=1")>-1)
 exp=[nd[1],
   "90.000",
@@ -60,7 +61,7 @@ exp=[nd[1],
   "212",
   "9"
 ];
-
+/* FOXPOP.CO.UK */
 if(ls.indexOf(nd[2]+"&d=2")>-1)
 exp=[nd[2],
   "70.000",
@@ -79,11 +80,30 @@ exp=[nd[2],
   "289",
   "0"
 ];
+/* PHOSFOR.CO */
+if(ls.indexOf(nd[3]+"&d=3")>-1)
+exp=[nd[3],
+  "50.000",
+  "2013",
+  "16-11-2021",
+  "16-11-2022",
+  "26",
+  "41",
+  "48%",
+  "242.018",
+  "4",
+  "4.1",
+  "26.875.410",
+  "6",
+  "8",
+  "43",
+  "0"
+];
 
 /* Title */
 document.title = exp[0]+" - Nama Business Registrant";
 
-var ket = '<h2>Keterangan :</h2><ul class=square><li>HISTORY = Riwayat domain<li>REGISTER = Waktu pendaftaran domain<li>EXPIRED = Waktu expired domain (dapat diperpanjang)<li>DA = Domain Authority<li>PA = Page Authority<li>SS = Spam Score<li>BL = Backlink<li>MT = Moz Trust<li>MR = Moz Rank<li>AR = Ahrefs Rank<li>DR = Domain Rating<li>UR = Url Rating<li>BL EDU = Backlink dari website .edu (pendidikan)<li>BL GOV = Backlink dari website .gov (pemerintah)</ul>';
+var ket = '<h2>Keterangan :</h2><ul class="square"><li>HISTORY = Riwayat domain<li>REGISTER = Waktu pendaftaran domain<li>EXPIRED = Waktu expired domain (dapat diperpanjang)<li>DA = Domain Authority<li>PA = Page Authority<li>SS = Spam Score<li>BL = Backlink<li>MT = Moz Trust<li>MR = Moz Rank<li>AR = Ahrefs Rank<li>DR = Domain Rating<li>UR = Url Rating<li>BL EDU = Backlink dari website .edu (pendidikan)<li>BL GOV = Backlink dari website .gov (pemerintah)</ul>';
 
 /* Table */
 rwID.innerHTML='<div id="nexp"><table class="borderless tab1 text-center"><thead class="thead1"><tr><th scope="col"><b id="nama">'+exp[0]+'</b> &gt; Rp<b id="rp">'+exp[1]+'</b> <a href="https://wa.me/message/QC3K6M45LU7RI1" id="wa" target="_blank" rel="nofollow noopener">WA</a></th></tr></thead><tbody><tr><td><table class="borderless bordered tab-in"><thead><tr><th scope="col">HISTORY</th><th scope="col">REGISTER</th><th scope="col">EXPIRED</th></tr></thead><tbody><tr><td id="hi">'+exp[2]+'</td><td id="reg">'+exp[3]+'</td><td id="exp">'+exp[4]+'</td></tr></tbody></table></td></tr><tr><td><table class="borderless bordered tab-in"><thead><tr><th scope="col">DA</th><th scope="col">PA</th><th scope="col">SS</th><th scope="col">BL</th><th scope="col">MT</th><th scope="col">MR</th></tr></thead><tbody><tr><td id="da">'+exp[5]+'</td><td id="pa">'+exp[6]+'</td><td id="ss">'+exp[7]+'</td><td id="bl">'+exp[8]+'</td><td id="mt">'+exp[9]+'</td><td id="mr">'+exp[10]+'</td></tr></tbody></table></td></tr><tr><td><table class="borderless bordered tab-in"><thead><tr><th scope="col">AR</th><th scope="col">DR</th><th scope="col">UR</th><th scope="col">BL EDU</th><th scope="col">BL GOV</th></tr></thead><tbody><tr><td id="ar">'+exp[11]+'</td><td id="dr">'+exp[12]+'</td><td id="ur">'+exp[13]+'</td><td id="edu">'+exp[14]+'</td><td id="gov">'+exp[15]+'</td></tr></tbody></table></td></tr></tbody></table>'+ket+'</div>';
