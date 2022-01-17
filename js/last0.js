d3888bfa0548dda6bbdd55b2407a3036cc5af03d
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-01-17 15:06:19 +0700

    Create last0.js

---
+++ js/last0.js
@@ -0,0 +1,9 @@
+/* Preloader */
+setTimeout(function() {
+  const l = doc.getElementById("preloader").style,
+    s = (s, p) => l.setProperty(s, p),
+    t = "transition",
+    w = "4.5s",
+    v = "visible";
+  s("-webkit-" + t, w), s("-moz-" + t, w), s("-ms-" + t, w), s("-o-" + t, w), s(t, w), s("opacity", "0"), s("overflow", v), s("visibility", "hidden"), doc.getElementsByTagName("body")[0].style.overflow = v
+}, 1500);
