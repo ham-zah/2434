42a584cf81750edacf038f567921a03fd89b7c62
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-01-18 21:25:13 +0700

    Create gen0.js

---
+++ js/gen0.js
@@ -0,0 +1,20 @@
+/* Gen Setting */
+var top_info = '<p>Domain .com Rp65.000 stok terbatas setiap harinya</p><a href="https://hamdaniazzah.my.id">Order Sekarang &raquo;</a>',
+    footyear = '2022 &middot; ';
+
+/* Gen Variable */
+var getheader = doc.getElementsByTagName("header")[1],
+    getfoota = doc.getElementById("foota");
+
+/* Home Function */
+function addAfter(newNode,existingNode) {existingNode.parentNode.insertBefore(newNode,existingNode.nextSibling)}
+
+/* Info */
+let createinfo=doc.createElement("div");
+createinfo.innerHTML='<div class="top-info text-center">'+top_info+'</div>';
+addAfter(createinfo,getheader);
+
+/* Copyright Year */
+var createfootyear = doc.createElement("span");
+createfootyear.innerHTML=footyear;
+getfoota.parentNode.insertBefore(createfootyear,getfoota);
