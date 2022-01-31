62044be14dfe275701e81fcc98be1528d1a5e618
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-01-31 17:10:14 +0700

    Update and rename home18.js to home19.js

--- js/home19.js
+++ js/home19.js
@@ -50,7 +50,7 @@ for(var trank=[
   "7%",
   "4.904",
   "/?exp=rachmadbagus.com&d=5",
-  "Rp90.000"
+  "Rp100.000"
 ]
 ],i=0;i<trank.length;i++){const a=doc.createElement("tr");for(var j=0;j<trank[i].length;j++){const e=doc.createElement("td");e.innerHTML=trank[i][j],a.appendChild(e)}getTrank.appendChild(a)}
 
