c7bb335e8e0d2301f2485364ca6ba9355b67b33d
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-02-04 14:11:35 +0700

    Update and rename home21.js to home22.js

--- js/home22.js
+++ js/home22.js
@@ -1,5 +1,5 @@
 /* Home Setting */
-var NamaSold = '<span class="bb_yellow">33</span>';
+var NamaSold = '<span class="bb_yellow">35</span>';
 
 /* Home Variable */
 var getTrank = doc.getElementById("t-ranked"),
