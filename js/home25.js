4fe21062424522c56157e1ae9b24426d64efbe81
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-02-06 14:39:45 +0700

    Update and rename home24.js to home25.js

--- js/home25.js
+++ js/home25.js
@@ -1,5 +1,5 @@
 /* Home Setting */
-var NamaSold = '<span class="bb_yellow">37</span>';
+var NamaSold = '<span class="bb_yellow">39</span>';
 
 /* Home Variable */
 var getTrank = doc.getElementById("t-ranked"),
