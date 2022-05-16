79290c10ea893f9ead15022cefacc25ecdc42f67
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-05-16 16:46:09 +0700

    Update and rename home84.js to home85.js

--- js/home85.js
+++ js/home85.js
@@ -1,5 +1,5 @@
 /* Home Setting */
-var NamaSold = '<span class="bb_yellow">88</span>';
+var NamaSold = '<span class="bb_yellow">92</span>';
 
 /* Home Variable */
 var getTreg0 = doc.querySelector(".t-reg"),
