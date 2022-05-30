f0fb988a072476067280d74b564f3c91514fb6d1
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2022-05-30 10:35:12 +0700

    Update and rename test9.js to test10.js

---
+++ test10.js
@@ -0,0 +1,11 @@
+let d=doc.createElement('div');
+d.innerHTML= `
+
+<h1>p1</h1>
+<p>p2</p>
+<p>p3</p>
+<a href="#">URL</a>
+
+`;
+
+kmain.appendChild(d);
--- test9.js
+++
@@ -1,8 +0,0 @@
-var article=[`
-
-<h1>p1</h1>
-<p>p2</p>
-<p>p3</p>
-<a href="#">URL</a>
-
-`];
